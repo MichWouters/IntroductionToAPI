0c055dbf55cf660ef49823d252db40121b616620
Michiel Wouters <michiel_woute64@hotmail.com>
2021-05-12 00:24:11 +0200

    End of lesson

--- Angular/MyFirstApi-Angular/src/app/app.component.ts
+++ Angular/MyFirstApi-Angular/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account.service';
+import { User } from './User';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MyFirstApi-Angular';
+  constructor(private AccountService: AccountService) { }
+
+  ngOnInit(): void {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user') || '{}');
+    this.AccountService.setCurrentUser(user);
+  }
 }
