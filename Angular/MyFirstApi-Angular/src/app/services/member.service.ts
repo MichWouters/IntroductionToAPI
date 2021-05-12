57ade75940c63cb85d7497be14f65209af0713ff
Michiel Wouters <michiel_woute64@hotmail.com>
2021-05-12 10:40:35 +0200

    End of lesson

--- Angular/MyFirstApi-Angular/src/app/services/member.service.ts
+++ Angular/MyFirstApi-Angular/src/app/services/member.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Member } from '../Member';
 
 @Injectable({
@@ -8,13 +8,19 @@ import { Member } from '../Member';
 export class MemberService {
   baseUrl = 'https://localhost:44388/api/users/members';
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: `Bearer ${JSON.parse(localStorage.getItem('user') || '{}').token}`
+    })
+  };
+
   constructor(private httpClient: HttpClient) { }
 
   getMembers() {
-    return this.httpClient.get<Member[]>(this.baseUrl);
+    return this.httpClient.get<Member[]>(this.baseUrl, this.httpOptions);
   }
 
   getMember(id: number) {
-    return this.httpClient.get<Member>(`this.baseUrl/${id}`);
+    return this.httpClient.get<Member>(`this.baseUrl/${id}`, this.httpOptions);
   }
 }
